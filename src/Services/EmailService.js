2ce894c8021b9a73422369a9fca7b35cb153afc1
Alphabeee <bob20031002@gmail.com>
2023-02-07 15:44:17 +0800

    fix EmailService

--- src/Services/EmailService.js
+++ src/Services/EmailService.js
@@ -1,9 +1,9 @@
 const dotenv = require("dotenv")
 dotenv.config()
-
+const express = require('express')
 const jwt = require("jsonwebtoken")
 const mailer = require("nodemailer")
-
+const router = express.Router()
 var transporter = nodemailer.createTransport({
 	host:'smtp.mailservice.com',
     secureConnection: true, 
@@ -13,8 +13,7 @@ var transporter = nodemailer.createTransport({
     	pass: pass,
   	},
 });
-
-
+   
 async function SendVerifyEmail(email, id){
     let token = jwt.sign({
         id: id
@@ -23,13 +22,26 @@ async function SendVerifyEmail(email, id){
 	var mailOptions = {
     	from: 'gmail帳號', // sender address
     	to: `${email}`, // list of receivers
-    	subject: '師大資工營', // Subject line
+    	subject: '師大資工營驗證碼', // Subject line
     	text: `您的驗證碼為${token}`, // plain text body
 	};
     //dunno why but even the test account doesnt want to work 
 	transporter.sendMail(mailOptions)
 }
 
+async function SendLoginEmail(email,id){
+	router.get('/',async(req,res)=>{
+		let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddressw
+		var mailOptions = {
+			from:'gmail',
+			to:`${email}`,
+			subject:'師大資工營登入通知',
+			text:`您已在 ${ip} 登入了`,
+		}
+		transporter.sendMail(mailOptions)
+	}
+}
 module.exports = {
-    SendVerifyEmail: SendVerifyEmail
+    SendVerifyEmail: SendVerifyEmail,
+	SendLoginEmail: SendLoginEmail,
 }
